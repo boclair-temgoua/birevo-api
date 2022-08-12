e4c387e02ff99f866ae36841c42707b7a23f1bab
Boclair Temgoua <temgoua2000@gmail.com>
2022-08-12 18:41:20 +0200

    fix(query): fix bug query

--- src/infrastructure/utils/commons/generate-number.ts
+++ src/infrastructure/utils/commons/generate-number.ts
@@ -1,9 +1,11 @@
 import { generateUUID } from './generate-uuid';
-import murmurhash2_x86_32 from 'number-generator/lib/murmurhash2_x86_32';
+import { murmurhash2_x86_32 } from 'number-generator';
 
 export const generateNumber = (length: number) => {
   let result = '';
-  const characters = `0123456789${murmurhash2_x86_32(generateUUID())}`;
+
+  const generator = murmurhash2_x86_32(`${generateUUID}`);
+  const characters = `0123456789${generator}`;
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
